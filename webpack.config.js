062d87d48518021c8ed1cb70ce5377d15adbdfcc
Bolander <kbolander@CHI-KBOLAND-MAC.local>
2017-07-31 10:17:36 -0500

    updated wepack version and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
+const path = require('path');
+
 module.exports = {
 	entry: './src/app.js',
 	output: {
-		path: './dist',
-		filename:'app.bundle.js'
+		path: path.resolve(__dirname, "dist"),
+		filename: 'app.bundle.js'
 	},
 	module: {
-		loaders: [{
-			exclude: '/node_modules/',
-			loader: 'babel-loader',
-			query: {
-				presets: ['es2015']
+		rules: [
+			{
+				exclude: '/node_modules/',
+				loader: 'babel-loader',
+				options: {
+					presets: ['es2015']
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
 			}
-		}]
+		]
 	}
 }
